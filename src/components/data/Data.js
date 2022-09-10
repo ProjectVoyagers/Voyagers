edda07b3638a053e4f67279a18f8821e908f3f6c
Siamias202 <84383708+Siamias202@users.noreply.github.com>
2022-09-10 11:45:16 +0600

    Add files via upload

---
+++ src/components/data/Data.js
@@ -0,0 +1,53 @@
+import imgOne from '../../Assets/images/blanket.png';
+import imgTwo from '../../Assets/images/bags and kits.png';
+import imgThree from '../../Assets/images/pillow.png';
+import imgFour from '../../Assets/images/umbrella.png';
+
+
+const data={
+    products:[
+        {
+            id:1,
+            img:imgOne,
+            title: 'Blanket',
+            desc:'Good Product',
+            price:80,
+
+        },
+        {
+            id:2,
+            img:imgTwo,
+            title: 'Bags and Kits',
+            desc:'Good Product',
+            price:90,
+
+        },
+        {
+            id:3,
+            img:imgThree,
+            title: 'Pillow',
+            desc:'Good Product',
+            price:60,
+
+        },
+        {
+            id:4,
+            img:imgFour,
+            title: "Umbrella",
+            desc:'Good Product',
+            price:100,
+
+        }
+        ,
+        {
+            id:5,
+            img:"https://imgs.search.brave.com/OULk4ev7UTn2hCaB4HPzEAmZtbGOaW-5CE-dYlVEfY8/rs:fit:390:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5I/N1hCUG15MlVPaXJP/THk4WU1TbzRRSGFJ/XyZwaWQ9QXBp",
+            title:"Headphones",
+            desc:"Good Product",
+            price:200,
+        }
+
+    ]
+}
+
+export default data;
