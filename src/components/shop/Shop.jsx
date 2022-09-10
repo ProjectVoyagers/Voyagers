88e7231d5bb86c2dd14ba60730940cddf062b2c6
Siamias202 <84383708+Siamias202@users.noreply.github.com>
2022-09-10 11:49:30 +0600

    Add files via upload

---
+++ src/components/shop/Shop.jsx
@@ -0,0 +1,32 @@
+import React,{} from "react";
+import { CartProvider } from "react-use-cart";
+
+import CardDetails from "../cards/CardDetails";
+import data from "../data/Data";
+import Cart from "../cart/Cart";
+
+
+export const Shop = () => {
+ 
+   
+    return (
+        <>
+            <CartProvider>
+                <div className='container-fluid'>
+                    <h3 className='text-center mt-5 text-uppercase'>Shop Page</h3>
+                    <div className="container py-4">
+                        <div className="row">
+                            {data.products.map((item, index) => {
+                                return (
+                                    <CardDetails img={item.img} title={item.title} desc={item.desc} price={item.price} item={item} key={index} />
+                                )
+                            })}
+
+                        </div>
+                    </div>
+                </div>
+                <Cart />
+            </CartProvider>
+        </>
+    );
+};
