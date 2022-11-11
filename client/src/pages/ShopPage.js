a88fbe429e504793a57b2740cf83464f6ed7279c
unknown <rakinuzzaman@gmail.com>
2022-11-11 11:40:51 +0600

    page created with shop and navbar

---
+++ client/src/pages/ShopPage.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar/Navbar";
+import Shop from "../components/shop/Shop.js";
+
+const Packages = () => {
+    return(
+        <>
+            <Navbar />
+            <Shop />
+        </>
+    )
+}
+
+export default Packages;
