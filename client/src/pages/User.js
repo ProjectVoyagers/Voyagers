16f976192f987595d4306e8ede0add5dd15122f9
rakinrkz <rakinuzzaman@gmail.com>
2022-12-29 21:31:42 +0600

    user page

---
+++ client/src/pages/User.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../components/userProfile/css/animate.min.css";
+import "../components/userProfile/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
+import "../components/userProfile/css/demo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import UserLayout from "layouts/userProfile.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/user" render={(props) => <UserLayout {...props} />} />
+      <Redirect from="/" to="/user/userProfile" />
+    </Switch>
+  </BrowserRouter>
+);
