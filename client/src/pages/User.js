b5ac0562ad016bcf67717fa138f7c57cce971513
rakinrkz <rakinuzzaman@gmail.com>
2022-12-30 14:22:39 +0600

    user modify

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
