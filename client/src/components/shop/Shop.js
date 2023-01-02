b452a405bb6a0e0353c5a14c96dcdb0940fc687f
rakinrkz <rakinuzzaman@gmail.com>
2023-01-03 01:36:23 +0600

    fixed shop styles

--- client/src/components/shop/Shop.js
+++ client/src/components/shop/Shop.js
@@ -4,7 +4,8 @@ import { CartProvider } from "react-use-cart";
 import CardDetails from "../cards/CardDetails";
 import data from "../data/Data";
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import './shop.css';
+    
 const Shop = ({ props }) => {
 
 
@@ -12,23 +13,26 @@ const Shop = ({ props }) => {
 
     return (
         <>
-            <CartProvider>
-                <div className='container-fluid'>
-                    <div className="nav-content">
-                        
-                    </div>
-                    <div className="container py-4">
-                        <div className="row">
-                            {data.products.map((item, index) => {
-                                return (
-                                    <CardDetails img={item.img} title={item.title} desc={item.desc} price={item.price} item={item} key={index} />
-                                )
-                            })}
+            <div className="shop_wrapper">
+                <h1 className="shop_header"> Shop </h1>
+                <CartProvider>
+                    <div className='container-fluid'>
+                        <div className="nav-content">
+                            
+                        </div>
+                        <div className="container py-4">
+                            <div className="row">
+                                {data.products.map((item, index) => {
+                                    return (
+                                        <CardDetails img={item.img} title={item.title} desc={item.desc} price={item.price} item={item} key={index} />
+                                    )
+                                })}
 
+                            </div>
                         </div>
                     </div>
-                </div>
-            </CartProvider>
+                </CartProvider>
+            </div>                
         </>
     );
 };
