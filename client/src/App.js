7c2f6533b60149054bb795cacc1e75ee03ded7f5
rakinrkz <rakinuzzaman@gmail.com>
2023-01-02 18:05:26 +0600

    itinerary styles fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./components/auth/login/login";
-// import Register from "./components/auth/signup/Register";
+import Register from "./components/auth/signup/Register";
 import LandingPage from "./pages/landingPage";
 import TravelPkgDesc from "./components/travel_pkg_desc/travel_pkg_desc.js";
 
@@ -28,16 +28,16 @@ const App = (props) => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route exact path="/" element={<LandingPage />}> </Route>
+        <Route exact path="/" element={<LandingPage />}> </Route>
         <Route exact path="/login" element={<Login />}> </Route>
         <Route path="/register" element={<Register />}> </Route>
         <Route path="/travelPackages" element={<Packages />}> </Route>
         <Route path="/travelPkgDesc" element={<TravelPkgDesc />}> </Route>
         <Route exact path="/shop" element={<Shop />}> </Route>        
-        <Route exact path="/cart" element={<Checkout />}> </Route> */}
-        {/* <Route exact path="/blogs" element={<Posts />}> </Route> */}
-        {/* <Route exact path="/thanks" element={<Thanks />} > </Route> */}
-        {/* <Route exact path="/user" element={<UserPage />}> </Route> */}
+        <Route exact path="/cart" element={<Checkout />}> </Route> 
+        <Route exact path="/blogs" element={<Posts />}> </Route>
+        <Route exact path="/thanks" element={<Thanks />} > </Route>
+        <Route exact path="/user" element={<UserPage />}> </Route>
 
         <Route path="/cities" element={<AllCities />}> </Route>
         <Route exact path="/city" element={<CityMap />}> </Route>
